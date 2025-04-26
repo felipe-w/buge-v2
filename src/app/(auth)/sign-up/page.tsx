0fe83c581150ccee7dd06841f30df115c9c98e51
Felipe Wanderley <felipegw@gmail.com>
2025-04-26 17:06:40 -0300

    Refactor authentication pages to use Suspense and separate form components for improved loading handling and code organization.

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,45 +1,13 @@
-"use client";
-
-import { signUpWithEmailOTP } from "@/server/actions/user-actions";
-import Form from "next/form";
-import { useActionState } from "react";
-
-import { AlertError } from "@/components/ui/alert";
-import { SubmitButton } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import Link from "next/link";
+import Loading from "@/app/loading";
+import SignUpForm from "@/components/auth/sign-up-form";
+import { Suspense } from "react";
 
 export default function SignUpPage() {
-  const [state, formAction, isPending] = useActionState(signUpWithEmailOTP, {
-    success: false,
-    message: "",
-    errors: undefined,
-  });
-
   return (
     <>
-      <Form className="p-6 md:p-8" action={formAction}>
-        <div className="flex flex-col gap-6">
-          <div className="flex flex-col items-center text-center">
-            <h1 className="text-2xl font-bold">Cadastre-se</h1>
-            <p className="text-muted-foreground text-balance">Crie sua conta para continuar</p>
-          </div>
-          {state.errors && <AlertError description={state.errors.server && state.errors.server[0]} />}
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" name="email" type="email" placeholder="exemplo@email.com" required />
-          </div>
-          <SubmitButton status={isPending ? "pending" : "idle"}>Cadastrar</SubmitButton>
-
-          <div className="text-center text-sm">
-            Já tem uma conta?{" "}
-            <Link href="/sign-in" className="underline underline-offset-4">
-              Faça login
-            </Link>
-          </div>
-        </div>
-      </Form>
+      <Suspense fallback={<Loading />}>
+        <SignUpForm />
+      </Suspense>
     </>
   );
 }
---
+++ src/components/auth/sign-up-form.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Form from "next/form";
+import { useActionState } from "react";
+
+import { AlertError } from "@/components/ui/alert";
+import { SubmitButton } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { signUpWithEmailOTP } from "@/server/actions/user-actions";
+import Link from "next/link";
+
+export default function SignUpForm() {
+  const [state, formAction, isPending] = useActionState(signUpWithEmailOTP, {
+    success: false,
+    message: "",
+    errors: undefined,
+  });
+
+  return (
+    <Form className="p-6 md:p-8" action={formAction}>
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-col items-center text-center">
+          <h1 className="text-2xl font-bold">Cadastre-se</h1>
+          <p className="text-muted-foreground text-balance">Crie sua conta para continuar</p>
+        </div>
+        {state.errors && <AlertError description={state.errors.server && state.errors.server[0]} />}
+        <div className="grid gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" name="email" type="email" placeholder="exemplo@email.com" required />
+          {state.errors?.email && <p className="text-destructive text-xs font-medium">{state.errors.email[0]}</p>}
+        </div>
+        <SubmitButton status={isPending ? "pending" : "idle"}>Cadastrar</SubmitButton>
+
+        <div className="text-center text-sm">
+          Já tem uma conta?{" "}
+          <Link href="/sign-in" className="underline underline-offset-4">
+            Faça login
+          </Link>
+        </div>
+      </div>
+    </Form>
+  );
+}
